f41a1cd3bc5c730caf098793d072b62fa2bf555a
11800222 <545423690@qq.com>
2020-04-24 19:24:58 +0800

    优化scheduler

--- plan/src/main/java/uncle/drew/repayment/plan/domain/model/plan/Scheduler.java
+++ plan/src/main/java/uncle/drew/repayment/plan/domain/model/plan/Scheduler.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.time.DateUtils;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Optional;
 
 
 public abstract class Scheduler {
@@ -25,23 +26,29 @@ public abstract class Scheduler {
      * 模板方法模式，由该方法定义算法的骨架，具体某一步骤的实现则由子类定义
      * 好处是，在统一隔离了不变处（算法骨架）与易变处（具体步骤实现）的同时，提供了扩展点。
      */
-    private ArrayList<RepayPlanCycle> generateSchedule(int totalCycleAmount, Date beginDate, Date endDate) {
+    public ArrayList<RepayPlanCycle> generateSchedule(Date beginDate, Date endDate, Integer totalCycleAmount) {
         TimeSpan timeSpan = new TimeSpan(beginDate, endDate);
         ArrayList<RepayPlanCycle> repayPlanCycles = new ArrayList<RepayPlanCycle>();
         Date currentDay = timeSpan.getBegin();
         while (!currentDay.after(timeSpan.getEnd())) {
+            ArrayList<RepayPlanCycle> currentDayCycle = null;
             if (DateUtils.isSameDay(currentDay, new Date())) {
                 Date currentDayBeginTime = timeSpan.getBegin().after(scheduleLimits.getDayAllowBeginTime()) ? timeSpan.getBegin() : scheduleLimits.getDayAllowBeginTime();
                 if (timeSpan.getBegin().before(scheduleLimits.getDayAllowEndTime())) {
                     //由子类具体实现该一步骤
-                    repayPlanCycles.addAll(scheduleForFirstDay(currentDayBeginTime, scheduleLimits.getDayAllowEndTime(), totalCycleAmount));
+                    currentDayCycle = scheduleForFirstDay(currentDayBeginTime, scheduleLimits.getDayAllowEndTime(), totalCycleAmount);
                 }
             } else {
                 //由子类具体实现该一步骤
-                repayPlanCycles.addAll(scheduleForSingleDay(scheduleLimits.getDayAllowBeginTime(), scheduleLimits.getDayAllowEndTime(), totalCycleAmount));
+                currentDayCycle = scheduleForSingleDay(scheduleLimits.getDayAllowBeginTime(), scheduleLimits.getDayAllowEndTime(), totalCycleAmount);
+            }
+            if (currentDayCycle != null) {
+                repayPlanCycles.addAll(currentDayCycle);
+                if (totalCycleAmount != null) {
+                    totalCycleAmount = totalCycleAmount - currentDayCycle.size();
+                }
             }
             DateUtils.addDays(currentDay, 1);
-            totalCycleAmount = totalCycleAmount - repayPlanCycles.size();
         }
         return repayPlanCycles;
     }
